ea052d5f8626725988ed424325c2918d12d98158
José Aniceto <jAniceto@users.noreply.github.com>
2024-09-08 23:17:26 +0100

    Add card-decklist relations to the Deck model in the admin panel

--- mtg_decks/admin.py
+++ mtg_decks/admin.py
@@ -3,6 +3,19 @@ from mtg_decks.models import Card, Deck, CardMainboard, CardSideboard
 
 
 admin.site.register(Card)
-admin.site.register(Deck)
-admin.site.register(CardMainboard)
-admin.site.register(CardSideboard)
+
+
+# Define the CardMainboard and CardSideboard inline admin
+class CardMainboardInline(admin.TabularInline):  # or use StackedInline for a different layout
+    model = CardMainboard
+    extra = 0  # number of extra blank forms to display
+
+
+class CardSideboardInline(admin.TabularInline):  # or use StackedInline for a different layout
+    model = CardSideboard
+    extra = 0  # number of extra blank forms to display
+
+
+@admin.register(Deck)
+class DeckAdmin(admin.ModelAdmin):
+    inlines = [CardMainboardInline, CardSideboardInline]
