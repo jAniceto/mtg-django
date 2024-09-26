58adb14649523fda9200bce85ae3086e4deea7f1
José Aniceto <jAniceto@users.noreply.github.com>
2024-09-27 00:05:19 +0100

    Added decks to card detail page

--- mtg_cards/views.py
+++ mtg_cards/views.py
@@ -4,7 +4,10 @@ from mtg_decks.models import Card
 
 def card_detail(request, pk):
     """Card detail page"""
-    card = get_object_or_404(Card, pk=pk)
+    card = get_object_or_404(
+        Card.objects.prefetch_related('cardmainboard_set__mainboard', 'cardsideboard_set__sideboard'), 
+        pk=pk
+    )
 
     # price_data = {'labels': [], 'data': [], 'edition': []}
     # for price in card.prices.all():
@@ -12,24 +15,7 @@ def card_detail(request, pk):
     #     price_data['data'].append(price.tix)
     #     price_data['edition'].append(price.edition)
 
-    # # Get user collections containing this card
-    # if request.user.is_authenticated:
-    #     user_collections_with_card = Collection.objects.all().filter(user=request.user, cards__name=card.name)
-    #     user_card_mainboard_associations = CardDeckMainboardAssociation.objects.all().filter(deck__user=request.user, card=card)
-    #     user_card_sideboard_associations = CardDeckSideboardAssociation.objects.all().filter(deck__user=request.user, card=card)
-    #     collections = Collection.objects.all().filter(user=request.user)
-    # else:
-    #     user_collections_with_card = None
-    #     user_card_mainboard_associations = None
-    #     user_card_sideboard_associations = None
-    #     collections = None
-
     context = {
         'card': card,
-        # 'price_data': json.dumps(price_data),
-        # 'user_collections_with_card': user_collections_with_card,
-        # 'user_card_mainboard_associations': user_card_mainboard_associations,
-        # 'user_card_sideboard_associations': user_card_sideboard_associations,
-        # 'collections': collections,
     }
     return render(request, 'mtg_cards/card_detail.html', context)
